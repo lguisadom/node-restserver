8770cc8a7bdbd9b920e764b784c8dc11d476211c
Luis Guisado <lguisadom@gmail.com>
2022-04-04 10:03:31 -0500

    Class model y archivo publico

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 require('dotenv').config();
 
-const { config } = require('dotenv');
-const express = require('express')
-const app = express()
+const Server = require('./models/server');
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-});
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+const server = new Server();
+server.listen();
---
+++ models/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        // Middlewares
+        this.middlewares();
+
+        // Rutas de la aplicación
+        this.routes();
+    }
+
+    middlewares() {
+        // Directorio público
+        this.app.use( express.static('public') );
+    }
+
+    routes() {
+        this.app.get('/api', (req, res) => {
+            res.send('Hello World')
+        });
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Server is running on port ${this.port}`);
+        });
+    }
+}
+
+module.exports = Server;
