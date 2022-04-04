07c16814caa4a0f059236dee0487c4c9f17bbb43
Luis Guisado <lguisadom@gmail.com>
2022-04-04 17:29:35 -0500

    Modelo de Usuario

--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const { dbConnection } = require('../database/config');
 class Server {
 
     constructor() {
@@ -7,11 +8,18 @@ class Server {
         this.port = process.env.PORT;
         this.usuariosPath = '/api/usuarios';
 
+        // Conectar a la base de datos
+        this.connectDB();
+
         // Middlewares
         this.middlewares();
 
         // Rutas de la aplicación
         this.routes();
+    }
+
+    async connectDB() {
+        await dbConnection();
     }
 
     middlewares() {
